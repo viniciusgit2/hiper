4e3ee03344ef74ce922200a765394435caf4b7f3
viniciusgit2 <4xv6patelfin@gmail.com>
2024-04-08 09:38:27 -0300

    integrando com css

---
+++ src/pages/integrando-com-css/integracao.jsx
@@ -0,0 +1,7 @@
+
+//tentei usar via integracao.css nao foi ai de um copia e cola jogando pra global.css ai foi
+export default function integracao (){
+   return(<div className="vermelha">texto1</div>)
+
+   
+}
